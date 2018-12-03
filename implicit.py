9aa0d152e2015f729adbb59555b80104fcc6feb2
davisrbr <davisbr@live.unc.edu>
2018-12-02 22:12:51 -0500

    adds working draft of chang cooper

--- implicit.py
+++ implicit.py
@@ -45,24 +45,25 @@ def implicitL(W0,D,U,fBND,dx,dt,it,x0,xf):
     a[0 ] = (-1/(2*k*T*dx)*F[0] + D/dx**2)
     a[-1] = (-1/(2*k*T*dx)*F[-1] + D/dx**2)
 
-
     diagonals = [b, a, c] #put in list in order to create sparse matrix
     A = diags(diagonals, [0, 1, -1]).toarray() #construct sparse tridiagonal matrix
 
     for t in range(1,it):
         r = np.linalg.solve(A, W[1:-1,t-1])
-        #r = fBND(r) #enforce b.c.s
-        #input()
-        #print(r)
         W[1:-1,t] = r #set solution
 
     return W
 
 def chang_cooper(W0,D,U,fBND,dx,dt,it,x0,xf):
+    '''Implicit solver
+            W0 initial conditions
+            D diffusion term
+            U potential
+            fBND boundary variables
+            '''
     J = len(W0)
     W = np.zeros((J+2,it+1))
     alpha = (D*dt)/dx**2  #alpha is dt/(kT*dx)
-
 
     # I'm sure I could make this two lines, but I didn't want to think
     W[1:J+1,0] = W0
@@ -73,34 +74,24 @@ def chang_cooper(W0,D,U,fBND,dx,dt,it,x0,xf):
     # Setting force equal to slope between x+dx/50 and x-dx/50
     F = np.zeros(J+2)
     x = np.arange(x0-dx,xf+dx,dx)
-    F = -(U(x+dx/50)-U(x-dx/50))/(dx/100)
-
-    a = np.zeros(J)
-    b = np.zeros(J)
-    c = np.zeros(J)
-
-    w = -F[1:-1]/D * dx
-
-    #define new variables necessary for flux conservation
-    delta_m = np.power(w,-1) - np.power(np.exp(w)-1, -1)
-    W_m = (w/2)/(np.sinh(w/2)) #may need to be rewritten for computational use (overflow may occur)
-    W_plus = W_m * np.exp(w/2)
-    W_minus = W_m * np.exp(-w/2)
+    F = -(U(x+dx/100)-U(x-dx/100))/(dx/50)
 
 
-    for t in range(1,it-1):
-        a = alpha * W_plus
-        b = 1 + alpha * (W_plus+W_minus) #may be ignoring important aspect of m+/- 1/2
-        c = alpha * W_minus
+    w = -F*dx/D
+    delta = 1/w - 1/(np.exp(w) - 1)
 
-        #construct sparse tridiagonal matrix
-        diagonals = [b, a, c]
-        A = diags(diagonals, [0, 1, -1]).toarray()
+    #set up tridiagonals
+    c = dt*(-1/(2*k*T*dx)*F[1:-1]*(1-delta[1:-1]) - D/dx**2)
+    b = dt*(1/dt + (-F[:-2]*delta[1:-1]+F[2:]*(1-delta[2:]))/(2*k*T*dx) + (2*D)/dx**2)
+    a = dt*(1/(2*k*T*dx)*F[1:-1]*delta[2:] - D/dx**2)
+
+    diagonals = [b, a, c] #put in list in order to create sparse matrix
+    A = diags(diagonals, [0, 1, -1]).toarray() #construct sparse tridiagonal matrix
+
+
+    for t in range(1,it):
         r = np.linalg.solve(A, W[1:-1,t-1])
-
-        #set solution
-        W[1:-1,t] = r
-
+        W[1:-1,t] = r #set solution
 
     return W
 
@@ -146,8 +137,8 @@ def Ugiven(x):
 
 W0,D,U,fBND,dx,dt,it,x0,xf = gaussianSetup()
 
-W = implicitL(W0,D,U,fBND,dx,dt,it,x0,xf)
-# W = chang_cooper(W0,D,U,fBND,dx,dt,it,x0,xf)
+# W = implicitL(W0,D,U,fBND,dx,dt,it,x0,xf)
+W = chang_cooper(W0,D,U,fBND,dx,dt,it,x0,xf)
 
 def areaCalc(W):
     area = np.zeros(it)
